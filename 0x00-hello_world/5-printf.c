9f6de4ca16aa26982a8005d4dcf2945c92cd905b
TumeloNtsilo <tumelontsilo@Gmail.com>
2023-02-20 13:14:19 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 
 /**
  * main -  A program that prints exactly "with proper grammar, but the outcome is a piece of art,"  followed by new line
- *Return: 0 Always
+ * Return: Always 0.
  *
  */
 
@@ -10,5 +10,5 @@ int main(void)
 
 {
 	print("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
