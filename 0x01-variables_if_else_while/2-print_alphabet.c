a8448d83188b94e9ccb0f8c39e72e56a1602a9ff
TumeloNtsilo <tumelontsilo@Gmail.com>
2023-02-20 19:12:04 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio>
+
+/**
+ * main - A program that prints the alphabet
+ * Return: 0 (Success)
+ *
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
+
