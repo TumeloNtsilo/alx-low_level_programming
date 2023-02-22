c6b2f67d0053e7ee0ba9456589d98a65ca0f2c88
TumeloNtsilo <tumelontsilo@Gmail.com>
2023-02-21 16:14:49 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - function that prints the alphabet, in lowercase
+ * Return: 0 (Success)
+ *
+ */
+
+void print_alphabet(void)
+
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return (0);
+}
