d83da17a9cbc7fa888f0ea61a23144bb4744a690
TumeloNtsilo <tumelontsilo@Gmail.com>
2023-02-22 15:24:07 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic character
+ * @c - character checked
+ * Return: 1 if c is an alphabet and 0 if otherwise
+ *
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
+
