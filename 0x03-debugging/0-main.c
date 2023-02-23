def6f58b6ecce3be493462d842443e2202e0fadb
TumeloNtsilo <tumelontsilo@Gmail.com>
2023-02-23 15:15:05 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Tests function that prints if interger is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+i = 98;
+
+positive_or_negative
+return (0);
+}
