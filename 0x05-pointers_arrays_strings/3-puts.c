c36cdaed9a4dca30c1645763f45066f6929707e4
TumeloNtsilo <tumelontsilo@Gmail.com>
2023-02-27 16:21:59 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts -  prints a string
+ * @str: String printed
+ * Return: void
+ *
+ */
+
+void _puts(char *str)
+
+{
+
+	for (; *str != '\0'; str++)
+	{
+	_putchar(*str);
+	}
+	_putchar('\n');
+}
