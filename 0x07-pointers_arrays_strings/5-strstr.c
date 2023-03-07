5d681b6533161a068f7919c0f91923ab1b9e259b
TumeloNtsilo <tumelontsilo@Gmail.com>
2023-03-07 15:12:12 -0800

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - locates a substring
+ * @needle: the substring
+ * @haystack: the string
+ * Return: Always 0.
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+
+{
+	for (; *haystack != 0; haystack++)
+	{
+	char *one = haystack;
+	char *two = needle;
+
+	while (*one == *two && *two != '\0')
+	{
+	*one++;
+	*two++;
+	}
+	if (*two == '\0')
+	return (haystack);
+	}
+	return (NULL);
+}
