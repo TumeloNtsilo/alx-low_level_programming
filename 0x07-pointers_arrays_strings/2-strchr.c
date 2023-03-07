ee7dc7a5b690cc432671258fb0045a181145f25f
TumeloNtsilo <tumelontsilo@Gmail.com>
2023-03-07 12:12:39 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * _strchr -  locates a character in a string
+ * @s: string
+ * @c: first occurrence of the character
+ * Return: Always 0.
+ *
+ */
+
+char *_strchr(char *s, char c)
+
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+	if (s[i] == c)
+	return (s + i);
+	}
+	return (NULL);
+}
